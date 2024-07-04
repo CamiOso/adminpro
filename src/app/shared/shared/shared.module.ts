376cfdba42cc7b3136ec96f3dba9917a4a1c4a8b
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-07-04 06:25:54 -0500

    Se usa el routerLink pra el account setting

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
+
 
 
 
@@ -15,7 +19,8 @@ import { HeaderComponent } from '../header/header.component';
   ],
   imports: [
     CommonModule,
-    
+    RouterModule
+
   ],
   exports: [
     BreadcrumbsComponent,
