1fbbfd05631f2357ad85bc9f56ffe4c9baac76a6
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-07-04 06:12:59 -0500

    Creacion del servicio Settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,42 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './account-settings.component.css'
 })
 export class AccountSettingsComponent implements OnInit{
-   public linkTheme= document.querySelector('#theme');
-   public links?: NodeListOf<Element>;
-  constructor() { }
+
+
+  constructor(private settingsService:SettingsService) { }
 
   ngOnInit(): void {
 
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
-  }
+    this.settingsService.checkCurrentTheme();
+   }
 
 
   changeTheme(theme: string) {
+    this.settingsService.changeTheme(theme);
 
-   const url = `assets/css/colors/${theme}.css`;
-   this.linkTheme?.setAttribute('href', url);
-   localStorage.setItem('theme', url);
-   this.checkCurrentTheme();
+
 
   }
 
-  checkCurrentTheme(){
 
-    this.links?.forEach((elem:any)=>{
-      elem.classList.remove('working');
-      const btnTheme=elem.getAttribute('data-theme');
-      const btnThemeUrl=`assets/css/colors/${btnTheme}.css`;
-      const currentTheme=this.linkTheme?.getAttribute('href');
-
-      if(btnThemeUrl===currentTheme){
-        elem.classList.add('working');
-      }
-
-
-    })
-
-
-  }
 
 }
--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,14 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit{
 
-  public linkTheme= document.querySelector('#theme');
+
+
+  constructor(private settingsService: SettingsService) {
+
+
+  }
 
   ngOnInit(){
- 
-   const url= localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
 
 
-   this.linkTheme?.setAttribute('href', url);
 
 
 
