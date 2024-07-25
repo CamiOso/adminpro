043ef607a4c999f0dd6bfa0c1326f9f4fe183210
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-07-25 12:14:53 -0500

    Se usa el metodo Retry de un observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, retry } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrl: './rxjs.component.css'
 })
 export class RxjsComponent {
+
+  constructor(){
+    let i=-1;
+    const obs$=new Observable(observer=>{
+
+
+     const intervalo= setInterval(()=>{
+        i++;
+        observer.next(i);
+        if(i===4){
+          clearInterval(intervalo);
+          observer.complete();
+
+
+
+        }
+       if(i===2){
+        i=0;
+          observer.error('i llego al valor de 2');
+
+       }
+
+      },1000)
+    });
+
+    obs$.pipe(
+      retry(1)
+
+    ).subscribe({
+
+
+        next: value=>console.log(value),
+        error: error=>console.error(error),
+        complete: ()=>console.log('Terminado')
+
+
+
+   } );
+
+  }
 
 }
