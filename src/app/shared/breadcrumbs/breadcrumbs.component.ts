ba5c528674d53f28861d0b8aa5d4db37fc80e706
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-09 12:40:54 -0500

    Se realizan optimizaciones del Breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrl: './breadcrumbs.component.css'
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+
+  public title!: string;
+  public tituloSubs$:Subscription;
+
+  constructor(private router:Router,private route:ActivatedRoute) {
+
+
+   this.tituloSubs$= this.getArgumentosRuta().
+   subscribe( { next:
+    (data: ActivationEnd) =>
+      this.title = data.snapshot.data['titulo'] }
+
+  );
+
+
+   }
+
+   ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+
+   }
+
+
+
+   getArgumentosRuta(){
+
+    return this.router.events
+    .pipe(
+       filter( (event: any) =>
+          event instanceof ActivationEnd &&
+          event.snapshot.firstChild === null &&
+          event.snapshot.data != null) )
+
+   }
 
 }
