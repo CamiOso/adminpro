374e32b30746131fd68b26505bfc189d1699156c
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-15 09:54:03 -0500

    Se hace formulario de Registro con Reactive Forms

--- src/app/auth/auth/auth.module.ts
+++ src/app/auth/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
@@ -14,14 +14,18 @@ import { RegisterComponent } from '../register/register.component';
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
   exports: [
+
     LoginComponent,
     RegisterComponent,
-    FormsModule
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  public registerForm=this.fb.group({
+    nombre:['',[Validators.required,Validators.minLength(3)]],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.minLength(6)]],
+    password2:['',[Validators.required,Validators.minLength(6)]],
+    terminos:[false,Validators.required]
+  });
+
+  constructor(private fb:FormBuilder) {
+
+
+  }
+
+  crearUsuario(){
+    console.log(this.registerForm.value);
+
+  }
+
+
 
 }
