8155411d4683225240ae88927a7f6788001a3779
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-18 08:31:01 -0500

    Se realiza validación de nombre e email

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  public formSubmited=false;
 
   public registerForm=this.fb.group({
     nombre:['',[Validators.required,Validators.minLength(3)]],
@@ -22,8 +23,33 @@ export class RegisterComponent {
   }
 
   crearUsuario(){
+    this.formSubmited=true;
     console.log(this.registerForm.value);
 
+    if(this.registerForm.valid){
+      console.log('formulario valido');
+
+    }else{
+      console.log('formulario no valido');
+    }
+
+  }
+
+  campoNoValido(campo:string):boolean{
+
+    if(this.registerForm.get(campo)?.invalid && this.formSubmited){
+      return true;
+
+    }else{
+      return false;
+    }
+ 
+
+  }
+
+
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmited;
   }
 
 
